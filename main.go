1e8a1448d172dd88c706a8b6f3505ee0ff71f64c
Mansur <vakhabov.mans@gmail.com>
2025-11-29 01:29:43 +0300

    finished GetMyLocation

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	"github.com/m4nsur/weather-lrn/geo"
 )
 
 func main() {
-	fmt.Println("хелло")
+
 
 	city := flag.String("city", "", "Введите название города")
-	format := flag.Int("format", 1, "Формат вывода погоды")
+	// format := flag.Int("format", 1, "Формат вывода погоды")
 	flag.Parse()
-	fmt.Println(*city, *format)
+	geoData, err := geo.GetMyLocation(*city)
+	if err != nil {
+		println(err)
+	}
+	fmt.Println(geoData.City)
 }
