81f3b46d6716f69e02a04274860640a44b72f912
Mansur <vakhabov.mans@gmail.com>
2025-11-29 02:14:33 +0300

    finish getWeather

--- main.go
+++ main.go
@@ -5,17 +5,20 @@ import (
 	"fmt"
 
 	"github.com/m4nsur/weather-lrn/geo"
+	"github.com/m4nsur/weather-lrn/weather"
 )
 
 func main() {
-
-
 	city := flag.String("city", "", "Введите название города")
-	// format := flag.Int("format", 1, "Формат вывода погоды")
+	format := flag.Int("format", 1, "Формат вывода погоды")
 	flag.Parse()
 	geoData, err := geo.GetMyLocation(*city)
 	if err != nil {
 		println(err)
 	}
-	fmt.Println(geoData.City)
+	data, err := weather.GetWeather(*geoData, *format)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(data)
 }
---
+++ weather/weather.go
@@ -0,0 +1,36 @@
+package weather
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/m4nsur/weather-lrn/geo"
+)
+
+func GetWeather(geo geo.GeoData, format int) (string, error){
+	baseUrl, err := url.Parse("https://wttr.in/" + geo.City)
+	if err != nil {
+		return "", err
+	}
+	params := url.Values{}
+	params.Add("format", fmt.Sprint(format))
+	baseUrl.RawQuery = params.Encode()
+
+	resp, err := http.Get(baseUrl.String())
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != 200 {
+	return "", err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+	return "", err
+	}
+
+	return string(body), nil
+}
