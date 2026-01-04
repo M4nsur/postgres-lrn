365d91dba5495697f978ec82fe4f0b523d7c6d8d
Mansur <vakhabov.mans@gmail.com>
2026-01-04 23:11:54 +0300

    update comments for migration

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -3,5 +3,18 @@ CREATE TABLE users {
     phone_number: VARCHAR(100)
 }
 
+
+-- Создание новой миграции:
 -- migrate create -ext sql -dir migrations -seq exampleName
+--
+-- Где:
+--  -ext sql        -> формат файлов (.sql)
+--  -dir migrations -> директория для миграций
+--  -seq            -> использовать последовательную нумерацию
+--  exampleName     -> осмысленное имя миграции (например: create_users_table)
+
+-- Применение миграций:
 -- migrate -path migrations -database "PATH?sslmode=disable" up
+--
+-- PATH — строка подключения к БД (например: postgres://user:pass@localhost:5432/dbname)
+-- sslmode=disable используется для локальной разработк
